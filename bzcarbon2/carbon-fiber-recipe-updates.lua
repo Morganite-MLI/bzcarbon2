e42ba067b452e1b87d868b3689ef6f94c08af500
Morganite <not@this.address>
2026-04-08 00:43:34 -0500

    Increase amount of carbon fiber in LDS recipes when Space Exploration is installed

--- bzcarbon2/carbon-fiber-recipe-updates.lua
+++ bzcarbon2/carbon-fiber-recipe-updates.lua
@@ -3,7 +3,10 @@ local util = require("data-util");
 if util.me.use_fiber() then
   if not mods["space-age"] then
     util.replace_ingredient("low-density-structure", "plastic-bar", "carbon-fiber")
-    util.replace_ingredient("se-low-density-structure-beryllium", "plastic-bar", "carbon-fiber")
+    if mods["space-exploration"] then
+        util.set_ingredient("low-density-structure", "carbon-fiber", 10)
+    end
+    util.set_ingredient("se-low-density-structure-beryllium", "carbon-fiber", 10)
     util.add_ingredient("exoskeleton-equipment", "carbon-fiber", 10)
     util.replace_ingredient("combat-shotgun", "wood", "carbon-fiber")
   end
